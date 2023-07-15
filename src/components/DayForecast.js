84edb05a8e13e689c46548772440fc95a2a59fe3
Alexis Migniau <migniau.alexis@gmail.com>
2023-07-16 00:00:38 +0200

    feat(daily) : Ajout données

--- src/components/DayForecast.js
+++ src/components/DayForecast.js
@@ -30,12 +30,17 @@ const DayLabel = styled.h4`
     margin-bottom : 4px;
 `
 
+const GroupIcon = styled.div`
+    display : flex;
+    flex-direction : row;
+    padding-left : 20px;
+`
+
 function DayForecast({dailyData}) {
     
     const [nextDays, setNextDays] = useState([]);
 
     useEffect(() => {
-        console.log(dailyData)
         setNextDays(dailyData.time.map((day, index) => {
             if(dailyData.weathercode[index]) {
                 const dayString = (new Date(day)).toLocaleString('fr-FR', {  weekday: 'long' })
@@ -44,7 +49,9 @@ function DayForecast({dailyData}) {
                     time : index === 0 ? "Aujourd'hui" : dayString.charAt(0).toUpperCase() + dayString.slice(1) ,
                     temperature_min : dailyData.temperature_2m_min[index],
                     temperature_max : dailyData.temperature_2m_max[index],
-                    weathercode :  dailyData.weathercode[index]
+                    weathercode :  dailyData.weathercode[index],
+                    sunrise : (new Date(dailyData.sunrise[index])).getHours() + "h" + (new Date(dailyData.sunrise[index])).getMinutes() + "m",
+                    sunset : (new Date(dailyData.sunset[index])).getHours() + "h" + (new Date(dailyData.sunset[index])).getMinutes() + "m",
                 };
             } else {
                 return null;
@@ -60,6 +67,22 @@ function DayForecast({dailyData}) {
                 <Day key={`day-${index}`}>
                     <DayLabel>{day.time}</DayLabel>
                     <img alt={day.weathercode} src={getIconFromWeatherCode(day.weathercode, true)} width="50px" />
+                    <GroupIcon>
+                        <h4>{day.temperature_min} °C</h4>
+                        <img alt={"Température minimum"} src={`https://raw.githubusercontent.com/basmilius/weather-icons/dev/design/fill/final/thermometer-colder.svg`} width="50px" />
+                    </GroupIcon>
+                    <GroupIcon>
+                        <h4>{day.temperature_max} °C</h4>
+                        <img alt={"Température maximale"} src={`https://raw.githubusercontent.com/basmilius/weather-icons/dev/design/fill/final/thermometer-warmer.svg`} width="50px" />
+                    </GroupIcon>
+                    <GroupIcon>
+                        <h4>{day.sunrise}</h4>
+                        <img alt={"Lever du soleil"} src={`https://raw.githubusercontent.com/basmilius/weather-icons/dev/design/fill/final/sunrise.svg`} width="50px" />
+                    </GroupIcon>
+                    <GroupIcon>
+                        <h4>{day.sunset}</h4>
+                        <img alt={"Coucher du soleil"} src={`https://raw.githubusercontent.com/basmilius/weather-icons/dev/design/fill/final/sunset.svg`} width="50px" />
+                    </GroupIcon>
                 </Day>
             ))}
         </DayContainer>
--- src/components/Meteo.js
+++ src/components/Meteo.js
@@ -67,11 +67,11 @@ function Meteo({data, city}) {
             <CityData>
                 <CityHeader>
                     <h2>{city.name}</h2>
-                    <img style={{paddingLeft : "5px"}} alt={city.country} src={`https://hatscripts.github.io/circle-flags/flags/${city.country_code.toLowerCase()}.svg`} width="45" />
+                    <img style={{paddingLeft : "5px"}} alt={city.country} src={`https://hatscripts.github.io/circle-flags/flags/${city.country_code.toLowerCase()}.svg`} width="40" />
                 </CityHeader>
                 <CityDescripion>{[city.country, city.admin1, city.admin2, city.admin3, city.admin4].filter(a => a !== "" && a !== city.name).filter((value, index, array) => array.indexOf(value) === index).join(', ')}</CityDescripion>
             </CityData>
-            <img style={{marginTop : "-30px", marginBottom : "-30px"}} alt={data.current_weather.weathercode} src={getIconFromWeatherCode(data.current_weather.weathercode, data.current_weather.is_day)} width="200px" />
+            <img style={{marginTop : "-40px", marginBottom : "-50px"}} alt={data.current_weather.weathercode} src={getIconFromWeatherCode(data.current_weather.weathercode, data.current_weather.is_day)} width="150px" />
             <Temperature>{data.current_weather.temperature} C°</Temperature>
            <HourForecast hourlyData={data.hourly} />
             <DayForecast dailyData={data.daily} />
